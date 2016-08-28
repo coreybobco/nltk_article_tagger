6b718a4ac0bbe7ca19b9e86adcf0539aeacd3557
Corey Bobco <corey.bobco@gmail.com>
2016-08-28 17:04:25 -0500

    cleans tokens now before filtering

--- tagger.py
+++ tagger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import collections
 import inflect
 import json
 import os
@@ -11,14 +12,19 @@ class Tagger_Class:
     self.config = json.load(open("config.json"))
     return
 
-  def tokenize_part_of_speech(self, article):
-    words = nltk.word_tokenize(article)
-    pos_tokens = nltk.pos_tag(words)
-    return pos_tokens
+  def tag(self, article):
+    pos_tokens = self.tokenize_part_of_speech(article)
+    full_names = self.extract_names(pos_tokens)
+    pos_tokens = self.group_proper_nouns(pos_tokens)
+    # filtered_pos_tokens, multiword_proper_nouns = self.group_proper_nouns(pos_tokens)
+    filtered_pos_tokens = self.filter_and_clean(pos_tokens)
+    words_sorted_by_freq = self.sort_dict_by_frequency(self.get_word_frequencies(filtered_pos_tokens, full_names))
+    name_tags = self.get_name_tags(words_sorted_by_freq, full_names)
+    # place_tags = self.get_place_tags(words_sorted_by_freq)
+    # topic_tags = self.get_topic_tags(words_sorted_by_freq)
 
   def extract_names(self, pos_tokens):
-    #NLTK's name recognition will try to classify proper noun phrases as persons, organizations, places, or 'other' (GPE)
-    #However, it doesn't work that well except for persons, and I've added some additional rules to handle common misclassifications.
+    '''NLTK's name recognition will try to classify proper noun phrases as persons, organizations, places, or 'other' (GPE). However, it doesn't work that well except for persons, and I've added some additional rules to handle common misclassifications.'''
     name_recognition_tokens = nltk.ne_chunk(pos_tokens)
     name_filter = self.config['name_filter'] 
     full_names = list()
@@ -26,28 +32,69 @@ class Tagger_Class:
       if not isinstance(token, tuple) and token.label() == "PERSON":
           noun_phrase_as_list = list(token[0] for token in token.leaves())
           noun_phrase_list_copy = noun_phrase_as_list[:]
-          # noun_phrase_as_list = list()
-          # for word_tuple in token.leaves():
-          #   noun_phrase_as_list.append(word_tuple[0])
           for word in noun_phrase_list_copy:
-            if noun_phrase_as_list[0] == "Texas":
-              print(noun_phrase_as_list)
-              print(word in name_filter)
             if word in name_filter:
               noun_phrase_as_list.remove(word)
           if len(noun_phrase_as_list) > 0 and noun_phrase_as_list[-1] != "County":
+            last_word = noun_phrase_as_list[-1]
+            if len(last_word) > 2 and last_word[-2:] == "'s":
+              # Strip the 's from this name'
+              noun_phrase_as_list[-1] = last_word[:-2]
             noun_phrase = " ".join(noun_phrase_as_list)
-            full_names.append(noun_phrase)
-    print(full_names)
+            if not noun_phrase in full_names:
+              skip_adding_name = False
+              for index, name in enumerate(full_names):
+                if name in noun_phrase:
+                  # Use the longer 'full' name instead
+                  full_names[index] = name
+                  skip_adding_name = True
+                elif noun_phrase in name:
+                  skip_adding_name = True
+              if not skip_adding_name:
+                full_names.append(noun_phrase)
     return full_names
 
+  def filter_and_clean(self, pos_tokens):
+    #Filter out tokens (words, punctuation) by part of speech or by checking against list of tokens
+    #Clean extraneous punctuation from tokens
+    filtered_pos_tokens = list()
+    punct_tokens = self.config['punct_tokens']
+    for token in pos_tokens:
+      print(token[0].strip("".join(punct_tokens)))
+      clean_token = (token[0].strip("".join(punct_tokens)), token[1])
+      if self.filter_pos(token[1]) and self.filter_token(token[0]):
+        filtered_pos_tokens.append(clean_token)
+    return filtered_pos_tokens
+
+  def filter_token(self, token):
+    '''Filter out tokens that are punctuation, linking verbs, have verbs, etc. that cannnot be filtered using NLTK's POS tagger. In theory the part-of-speech filter should eliminate all verbs, but things like quotation marks for (embedded) quotes can make it misclassify parts of speech'''
+    punct_tokens = self.config['punct_tokens']
+    articles = self.config["articles"]
+    linking_verbs = self.config["linking_verbs"]
+    aux_verbs = self.config["aux_verbs"]
+    abbreviations = self.config["abbreviations"]
+    if token in punct_tokens or token.lower() in linking_verbs or token.lower() in aux_verbs or token.lower() in articles or token in abbreviations:
+      return False
+    else:
+      return True
+
+  def filter_pos(self, pos):
+    '''POS filter will eliminate conjunctions, punctuation, prepositions, pronouns, 
+    determiners, modal auxiliary, comparative/superlative adjectives, ordinal adjectives, 
+    genitive marker ('s), item markers, existential there, foreign word, adverbs(?), verbs.
+    For complete list open python3 console and nltk.help.upenn_tagset()'''
+    pos_filter = self.config["pos_filter_parts"]
+    if pos in pos_filter:
+      return False
+    else:
+      return True
 
   def get_word_frequencies(self, pos_tokens, full_names):
     inflector = inflect.engine()
     frequency_dict = {}
     proper_noun_pos_list = ["NNP", "NNPS"]
     for token in pos_tokens:
-      added_to_dict = false
+      added_to_dict = False
       current_word = token[0]
       current_word_pos = token[1]
       if current_word_pos in proper_noun_pos_list:
@@ -55,19 +102,38 @@ class Tagger_Class:
         for name in full_names:
           if current_word in name or name in current_word:
             if not name in frequency_dict.keys():
-              frequency_dict[current_word] = 1
-              added_to_dict = true
+              frequency_dict[name] = 1
+              added_to_dict = True
             else:
-              frequency_dict[current_word] += 1
-              added_to_dict = true
+              frequency_dict[name] += 1
+              added_to_dict = True
       if not added_to_dict:
+        #Normalize words by singularizing plural nouns and lowercase mixed case words before adding to frequency dict
         if current_word_pos == "NNS":
           current_word = inflector.singular_noun(current_word)
-        if current_token[0] in frequency_dict:
-          frequency_dict[current_token] += 1
+        if not current_word.isupper() and not current_word.islower():
+          current_word = current_word[0].lower() + current_word[1:]
+        if current_word in frequency_dict:
+          frequency_dict[current_word] += 1
         else: 
-          frequency_dict[current_token] = 1
+          frequency_dict[current_word] = 1
     # print(frequency_dict)
+    return(frequency_dict)
+
+  def sort_dict_by_frequency(self, frequency_dict):
+    words_sorted_by_freq = {}
+    for word, frequency in frequency_dict.items():
+      if frequency in words_sorted_by_freq.keys():
+          words_sorted_by_freq[frequency].append(word)
+      else:
+          words_sorted_by_freq[frequency] = [word]
+    words_sorted_by_freq = collections.OrderedDict(sorted(words_sorted_by_freq.items(), reverse=True))
+    print("---------")
+    for frequency, wordlist in words_sorted_by_freq.items():
+      print(str(frequency) + " --> " +", ".join(wordlist))
+    print("---------")
+    return words_sorted_by_freq
+
 
   def group_proper_nouns(self, pos_tokens):
     '''Group nouns that occur right after each other as one word unit, i.e. "Associated Press", "news agency" and rebuild data structure 
@@ -91,60 +157,17 @@ class Tagger_Class:
         elif last_token_pos in proper_noun_filter and current_token_pos not in proper_noun_filter:
           if combo_token == ("", ""): 
             processed_pos_tokens.append(last_token)
-          # if len(combo_token[0].split(" ")) > 1:
-          #   multiword_proper_nouns.append(combo_token)
+          if len(combo_token[0].split(" ")) > 1:
+            processed_pos_tokens.append(combo_token)
           combo_token = ("","")
     return processed_pos_tokens #, multiword_proper_nouns
 
-  def filter(self, pos_tokens):
-    #Filter out tokens (words, punctuation) by part of speech or by checking against list of tokens    
-    filtered_pos = list()
-    for token in pos_tokens:
-      if self.filter_pos(token[1]) and self.filter_token(token[0]):
-        # print(pos_token)
-        filtered_pos.append(token)
-
-  def filter_token(self, token):
-    #Filter out tokens that are punctuation, linking verbs, have verbs, etc. that cannnot be filtered using NLTK's POS tagger
-    punct_tokens = self.config['punct_tokens']
-    if token[0] in punct_tokens:
-      if len(token) == 1:
-        return False
-      else:
-        token = token[1:]
-    if token[-1] in punct_tokens:
-      if len(token) == 1:
-        return False
-      else:
-        token = token[:-1]
-    articles = self.config["articles"]
-    linking_verbs = self.config["linking_verbs"]
-    aux_verbs = self.config["aux_verbs"]
-    abbreviations = self.config["abbreviations"]
-    if token.lower() in punct_tokens or token in linking_verbs or token in aux_verbs or token in articles:
-      return False
-    else:
-      return True
-
-  def filter_pos(self, pos):
-    '''POS filter will eliminate conjunctions, punctuation, prepositions, pronouns, 
-    determiners, modal auxiliary, comparative/superlative adjectives, ordinal adjectives, 
-    genitive marker ('s), item markers, existential there, foreign word, adverbs(?), verbs.
-    For complete list open python3 console and nltk.help.upenn_tagset()'''
-    pos_filter = self.config["pos_filter_parts"]
-    if pos in pos_filter:
-      return False
-    else:
-      return True
-
-  def tag(self, article):
-    pos_tokens = self.tokenize_part_of_speech(article)
-    full_names = self.extract_names(pos_tokens)
-    pos_tokens = self.group_proper_nouns(pos_tokens)
-    # filtered_pos_tokens, multiword_proper_nouns = self.group_proper_nouns(pos_tokens)
-    filtered_pos_tokens = self.filter(pos_tokens)
-    self.get_word_frequencies(filtered_pos_tokens, full_names)
-
+  def tokenize_part_of_speech(self, article):
+    # TODO fix Trump's so same as Trump
+    # article = article.replace("Trump's", "Drumpf")
+    words = nltk.word_tokenize(article)
+    pos_tokens = nltk.pos_tag(words)
+    return pos_tokens
 
 
 

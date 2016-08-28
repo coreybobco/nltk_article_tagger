dafe98de45f14c595ee218ace7edae698c1b36cf
Corey Bobco <corey.bobco@gmail.com>
2016-08-28 03:57:50 -0500

    fixed issue with name extraction

--- tagger.py
+++ tagger.py
@@ -24,68 +24,85 @@ class Tagger_Class:
     full_names = list()
     for token in name_recognition_tokens:
       if not isinstance(token, tuple) and token.label() == "PERSON":
-          print(token)
           noun_phrase_as_list = list(token[0] for token in token.leaves())
-          for word in noun_phrase_as_list:
+          noun_phrase_list_copy = noun_phrase_as_list[:]
+          # noun_phrase_as_list = list()
+          # for word_tuple in token.leaves():
+          #   noun_phrase_as_list.append(word_tuple[0])
+          for word in noun_phrase_list_copy:
+            if noun_phrase_as_list[0] == "Texas":
+              print(noun_phrase_as_list)
+              print(word in name_filter)
             if word in name_filter:
               noun_phrase_as_list.remove(word)
-          if len(noun_phrase_as_list) > 0:
+          if len(noun_phrase_as_list) > 0 and noun_phrase_as_list[-1] != "County":
             noun_phrase = " ".join(noun_phrase_as_list)
-            print(noun_phrase)
-    return pos_tokens
+            full_names.append(noun_phrase)
+    print(full_names)
+    return full_names
 
 
-  def get_word_frequencies(self, pos_tokens, multiword_proper_nouns):
+  def get_word_frequencies(self, pos_tokens, full_names):
     inflector = inflect.engine()
-    frequency_dictionary = {}
-    plural_noun_filter = ["NNS", "NNPS"]
+    frequency_dict = {}
+    proper_noun_pos_list = ["NNP", "NNPS"]
     for token in pos_tokens:
+      added_to_dict = false
       current_word = token[0]
-      if current_token[1] in plural_noun_filter:
-        current_word = inflector.singular_noun(current_word)
-        if len(token[0].split(" ")) == 1 and (token[1] == "NNP" or token[1] == "NNPS"):
-          # Add proper nouns that are part 
-          frequency_dictionary = frequency_dictionary
-      if current_token[0] in frequency_dictionary:
-        frequency_dictionary[current_token] += 1
-      else: 
-        frequency_dictionary[current_token] = 1
+      current_word_pos = token[1]
+      if current_word_pos in proper_noun_pos_list:
+        # See if the proper noun is part of a name, if so, just up the count for the full name to the dict
+        for name in full_names:
+          if current_word in name or name in current_word:
+            if not name in frequency_dict.keys():
+              frequency_dict[current_word] = 1
+              added_to_dict = true
+            else:
+              frequency_dict[current_word] += 1
+              added_to_dict = true
+      if not added_to_dict:
+        if current_word_pos == "NNS":
+          current_word = inflector.singular_noun(current_word)
+        if current_token[0] in frequency_dict:
+          frequency_dict[current_token] += 1
+        else: 
+          frequency_dict[current_token] = 1
+    # print(frequency_dict)
 
   def group_proper_nouns(self, pos_tokens):
-    #Group nouns that occur right after each other as one word unit, i.e. "Associated Press", "news agency" and
-    #rebuild data structure while also providing separate list
+    '''Group nouns that occur right after each other as one word unit, i.e. "Associated Press", "news agency" and rebuild data structure 
+    while also providing separate list'''
     proper_noun_filter = ["NNP", "NNPS"]
     processed_pos_tokens = list()
-    multiword_proper_nouns = list()
+    # multiword_proper_nouns = list()
     combo_token = ("","")
     for index, current_token in enumerate(pos_tokens):
       if index > 0:
         last_token = pos_tokens[index - 1]
-        last_pos = last_token[1]
-        current_pos = current_token[1]
-        if last_pos not in proper_noun_filter:
+        last_token_pos = last_token[1]
+        current_token_pos = current_token[1]
+        if last_token_pos not in proper_noun_filter:
           processed_pos_tokens.append(last_token)
-        elif last_pos in proper_noun_filter and current_pos in proper_noun_filter:
+        elif last_token_pos in proper_noun_filter and current_token_pos in proper_noun_filter:
           if combo_token == ("",""):
             combo_token = (last_token[0] + " " + current_token[0], current_token[1])
           else:
             combo_token = (combo_token[0] + " " + current_token[0], current_token[1])
-        elif last_pos in proper_noun_filter and current_pos not in proper_noun_filter: 
-          processed_pos_tokens.append(combo_token)
-          if len(combo_token[0].split(" ")) > 1:
-            multiword_proper_nouns.append(combo_token)
+        elif last_token_pos in proper_noun_filter and current_token_pos not in proper_noun_filter:
+          if combo_token == ("", ""): 
+            processed_pos_tokens.append(last_token)
+          # if len(combo_token[0].split(" ")) > 1:
+          #   multiword_proper_nouns.append(combo_token)
           combo_token = ("","")
-    # print(multiword_proper_nouns)
-    # print(nltk.ne_chunk(multiword_proper_nouns))
-    return processed_pos_tokens, multiword_proper_nouns
+    return processed_pos_tokens #, multiword_proper_nouns
 
   def filter(self, pos_tokens):
     #Filter out tokens (words, punctuation) by part of speech or by checking against list of tokens    
     filtered_pos = list()
-    for pos_token in pos_tokens:
-      if self.filter_pos(pos_token[1]) and self.filter_token(pos_token[0]):
+    for token in pos_tokens:
+      if self.filter_pos(token[1]) and self.filter_token(token[0]):
         # print(pos_token)
-        filtered_pos.append(pos_token)
+        filtered_pos.append(token)
 
   def filter_token(self, token):
     #Filter out tokens that are punctuation, linking verbs, have verbs, etc. that cannnot be filtered using NLTK's POS tagger
@@ -122,10 +139,11 @@ class Tagger_Class:
 
   def tag(self, article):
     pos_tokens = self.tokenize_part_of_speech(article)
-    self.extract_names(pos_tokens)
-    filtered_pos_tokens, multiword_proper_nouns = self.group_proper_nouns(pos_tokens)
-    # print(multiword_proper_nouns)
+    full_names = self.extract_names(pos_tokens)
+    pos_tokens = self.group_proper_nouns(pos_tokens)
+    # filtered_pos_tokens, multiword_proper_nouns = self.group_proper_nouns(pos_tokens)
     filtered_pos_tokens = self.filter(pos_tokens)
+    self.get_word_frequencies(filtered_pos_tokens, full_names)
 
 
 
